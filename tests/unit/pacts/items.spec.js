7f4c62e54464410d62af32ae16fad624ce0e10a1
Nida Dinç <20160808047@ogr.akdeniz.edu.tr>
2022-02-25 00:32:45 +0300

    add pact test for post

--- tests/unit/pacts/items.spec.js
+++ tests/unit/pacts/items.spec.js
@@ -38,5 +38,34 @@ pactWith({
             expect(response.data[0].text).toEqual('buy some milk')
 
         })
+
+        it('add todo list item correctly', async function () {
+            await provider.addInteraction({
+                state: 'add todo item successfully',
+                uponReceiving: 'a request for adding todo list item',
+                withRequest: {
+                    method: 'POST',
+                    path: '/item',
+                    body: eachLike({
+                        text: string('buy some milk')
+                    })
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8',
+                    },
+                    body: {
+                        data: eachLike({
+                            id: like(1),
+                            text: string('buy some milk')
+                        })
+                    }
+                }
+            })
+            const response = await api.addTodoItem([{text : "buy some milk"}])
+            expect(response.data[0].text).toEqual('buy some milk')
+
+        })
     })
 })
